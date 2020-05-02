896e522f5d19bcda290771faa6a7f42b4d539ba8
black-94 <19278347+black-94@users.noreply.github.com>
2020-05-03 02:45:47 +0800

    commit

--- src/main/java/com/black/repository/SERepository.java
+++ src/main/java/com/black/repository/SERepository.java
@@ -11,7 +11,7 @@ public class SERepository extends BaseRepository{
     public void insert(List<SEPo> list){
         String sql="insert into stock_exchange(code,name,country) values ";
         for (SEPo sePo : list) {
-            sql+=String.format("(%s,%s,%s)",sePo.getCode(),sePo.getName(),sePo.getCountry())+",";
+            sql+=String.format("('%s','%s','%s')",sePo.getCode(),sePo.getName(),sePo.getCountry())+",";
         }
         sql=sql.substring(0,sql.length()-1);
         jdbcTemplate.execute(sql);
