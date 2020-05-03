2d3fae43cbbf4689333dba58a3b0445893eb5212
black-94 <19278347+black-94@users.noreply.github.com>
2020-05-03 17:11:41 +0800

    commit

--- src/main/java/com/black/repository/ErrorRepository.java
+++ src/main/java/com/black/repository/ErrorRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class ErrorRepository extends BaseRepository{
 
     public void save(ErrorPo po){
-        String sql="insert into tb_error_log(type,msg,stack)values('%s','%s','%s')";
-        jdbcTemplate.execute(String.format(sql,po.getType(),po.getMsg(),po.getStack()));
+        String sql="insert into tb_error_log(type,msg,stack)values(?,?,?)";
+        jdbcTemplate.update(sql,po.getType(),po.getMsg(),po.getStack());
     }
 }
