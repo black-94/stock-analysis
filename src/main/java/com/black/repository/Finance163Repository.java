9351dba4e7e706bf6053aaa632539ada883ab21c
black-94 <19278347+black-94@users.noreply.github.com>
2020-05-16 18:07:45 +0800

    commit

--- src/main/java/com/black/repository/Finance163Repository.java
+++ src/main/java/com/black/repository/Finance163Repository.java
@@ -141,13 +141,13 @@ public class Finance163Repository {
     }
 
     public List<Finance163StockHistoryFinancePO> queryHistoryFinance(String code, String exchanger){
-        String url="http://quotes.money.163.com/f10/zycwzb_%s,season.html";
+        String url="http://quotes.money.163.com/f10/lrb_%s.html";
         String res = NetUtil.get(url,code);
         Document doc = Jsoup.parse(res);
         Elements elements = doc.select(".table_bg001.border_box.limit_sale.scr_table tr");
         Elements dates = elements.get(0).select("th");
-        Elements incomes = elements.get(4).select("td");
-        Elements profits = elements.get(10).select("td");
+        Elements incomes = elements.get(1).select("td");
+        Elements profits = elements.get(40).select("td");
 
         if(dates.size()<1){
             return new ArrayList<>();
