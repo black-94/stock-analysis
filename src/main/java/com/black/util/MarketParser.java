9ef6b39753112ea54cbfb4441f4325a1215619a0
shangzheng <shangzheng@pinduoduo.com>
2020-06-29 09:46:04 +0800

    commit

--- src/main/java/com/black/util/MarketParser.java
+++ src/main/java/com/black/util/MarketParser.java
@@ -12,11 +12,11 @@ public class MarketParser {
         map.put("sz.200","深B");
         map.put("sz.002","深中小");
         map.put("sz.300","深创");
-        map.put("sh.300","沪A");
-        map.put("sh.300","沪B");
-        map.put("sh.300","沪");
-        map.put("sh.300","深创");
-        map.put("sh.300","深创");
+        map.put("sh.601","沪A");
+        map.put("sh.602","沪A");
+        map.put("sh.603","沪A");
+        map.put("sh.900","沪B");
+        map.put("sh.688","沪科创");
         code2Market=Collections.unmodifiableMap(map);
     }
 }
