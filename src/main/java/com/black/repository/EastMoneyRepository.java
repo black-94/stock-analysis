6ced58229d989fd32de8fdaf3a58c8aa6f1b1c92
black-94 <19278347+black-94@users.noreply.github.com>
2020-07-12 01:06:08 +0800

    commit

--- src/main/java/com/black/repository/EastMoneyRepository.java
+++ src/main/java/com/black/repository/EastMoneyRepository.java
@@ -16,26 +16,27 @@ import java.util.stream.Collectors;
 @Repository
 public class EastMoneyRepository {
 
-    public List<StockInfoPo> queryAllStockCode(){
-        String url="http://quote.eastmoney.com/stock_list.html";
+    public List<StockInfoPo> queryAllStockCode() {
+        String url = "http://quote.eastmoney.com/stock_list.html";
         String res = NetUtil.get(url);
         Elements uls = Jsoup.parse(res).select("#quotesearch ul");
-        List<StockInfoPo> list=new ArrayList<>();
+        List<StockInfoPo> list = new ArrayList<>();
         for (Element e : uls) {
             String exchanger = e.previousElementSibling().selectFirst("a").attr("name");
             List<String> codes = e.select("ul li a").stream().map(a -> a.attr("href")).filter(StringUtils::isNoneBlank)
                     .distinct().collect(Collectors.toList());
-            codes.forEach(c->list.add(buildStockInfoPo(c,exchanger)));
+            codes.forEach(c -> list.add(buildStockInfoPo(c, exchanger)));
         }
         return list;
     }
 
-    private StockInfoPo buildStockInfoPo(String link, String exchanger){
-        link=StringUtils.remove(link,"http://quote.eastmoney.com/");
-        link=StringUtils.remove(link,".html");
-        link=link.substring(2);
+    private StockInfoPo buildStockInfoPo(String link, String exchanger) {
+        link = StringUtils.remove(link, "http://quote.eastmoney.com/");
+        link = StringUtils.remove(link, ".html");
+        link = link.substring(2);
         String sub = MarketParser.parse(exchanger, link);
-        return StockInfoPo.builder().code(link).exchanger(exchanger).subExchanger(sub).build();
+        return StockInfoPo.builder().code(link).exchanger(exchanger).subExchanger(sub)
+                .name("").biz("").openDay("").marketDay("").build();
     }
 
 }
--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -8,7 +8,7 @@ CREATE TABLE `stock_exchanger` (
 CREATE TABLE `stock_info` (
 	`id` bigint(20) NOT NULL AUTO_INCREMENT,
 	`code` varchar(20)  NOT NULL COMMENT '股票代码',
-	`name` varchar(50)  NOT NULL default '' COMMENT '股票名称',
+	`name` varchar(50)  NOT NULL COMMENT '股票名称',
 	`exchanger` varchar(20)  NOT NULL COMMENT '上市证券市场',
 	`subExchanger` varchar(20)  NOT NULL COMMENT '子板',
 	`biz` varchar(2000)  NOT NULL COMMENT '经营范围',
