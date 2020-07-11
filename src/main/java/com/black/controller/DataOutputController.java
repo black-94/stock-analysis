dbdb67e7dc2fb266f167b6d9951546f1c60a890b
black-94 <19278347+black-94@users.noreply.github.com>
2020-07-11 23:41:12 +0800

    commit

--- src/main/java/com/black/controller/DataOutputController.java
+++ src/main/java/com/black/controller/DataOutputController.java
@@ -1,12 +1,15 @@
 package com.black.controller;
 
+import com.black.jobs.Crawler;
 import com.black.po.StockExchangerPo;
 import com.black.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,21 +18,31 @@ import java.util.stream.Collectors;
 @RequestMapping("/")
 public class DataOutputController {
 
+    @Autowired
+    Crawler crawler;
+
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "中文测试";
     }
 
     @GetMapping("/seinit")
-    public String init(){
-        String[][] stockExchanges={
-                {"NASDAQ","纳斯达克","usa"},
-                {"NYSE","纽约证券交易所","usa"},
-                {"HKEX","香港证券交易所","hk"},
-                {"SSE","上海证券交易所","china"},
-                {"SZSE","深圳证券交易所","china"}
+    public String init() {
+        String[][] stockExchanges = {
+                {"NASDAQ", "纳斯达克", "usa"},
+                {"NYSE", "纽约证券交易所", "usa"},
+                {"HKEX", "香港证券交易所", "hk"},
+                {"SSE", "上海证券交易所", "china"},
+                {"SZSE", "深圳证券交易所", "china"}
         };
         List<StockExchangerPo> list = Arrays.stream(stockExchanges).map(e -> new StockExchangerPo(e[0], e[1], e[2])).collect(Collectors.toList());
+        return "ok";
+    }
+
+    @GetMapping("/job/{job}")
+    public String invokeJob(@PathVariable String job) throws Exception {
+        Method method = crawler.getClass().getMethod(job);
+        method.invoke(crawler);
         return "ok";
     }
 
