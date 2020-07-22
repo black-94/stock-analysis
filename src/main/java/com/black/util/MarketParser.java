226614d573222f43dfab2df6703f37c064c71a38
shangzheng <shangzheng@pinduoduo.com>
2020-07-22 22:49:04 +0800

    commit

--- src/main/java/com/black/util/MarketParser.java
+++ src/main/java/com/black/util/MarketParser.java
@@ -12,8 +12,10 @@ public class MarketParser {
     static {
         Map<String, String> map = new HashMap<>();
         map.put("000", "sz.深A");
-        map.put("200", "sz.深B");
+        map.put("001", "sz.深A");
         map.put("002", "sz.深中小");
+        map.put("003", "sz.深A");
+        map.put("200", "sz.深B");
         map.put("300", "sz.深创");
         map.put("600", "sh.沪A");
         map.put("601", "sh.沪A");
