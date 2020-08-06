42936029d8651824b58d9913a963c5bfd7174f27
shangzheng <shangzheng@pinduoduo.com>
2020-08-06 20:45:34 +0800

    commit

--- src/main/java/com/black/repository/StockPriceRepository.java
+++ src/main/java/com/black/repository/StockPriceRepository.java
@@ -4,16 +4,13 @@ package com.black.repository;
 import com.black.po.StockPricePo;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 @Mapper
 public interface StockPriceRepository{
 
     @Insert("""
-            insert into stock_price(code,open,cur,lastClose,high,low,volume,amount,updown,change,amplitude,date)
-            values (#{code},#{open},#{cur},#{lastClose},#{high},#{low},#{volume},#{amount},#{updown},#{change},#{amplitude},#{date})
+            insert into stock_price(code,open,close,high,low,volume,amount,updown,change,exchange,capital,date)
+            values (#{code},#{open},#{close},#{high},#{low},#{volume},#{amount},#{updown},#{change},#{exchange},#{capital},#{date})
             """)
     void insert(StockPricePo stockPricePo);
 }
