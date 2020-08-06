c1bed25adae3254a59522a72d7c11336ddaa39d0
shangzheng <shangzheng@pinduoduo.com>
2020-08-06 21:00:48 +0800

    commit

--- src/main/java/com/black/repository/StockPriceRepository.java
+++ src/main/java/com/black/repository/StockPriceRepository.java
@@ -9,8 +9,8 @@ import org.apache.ibatis.annotations.Mapper;
 public interface StockPriceRepository{
 
     @Insert("""
-            insert into stock_price(code,open,close,high,low,volume,amount,updown,change,exchange,capital,date)
-            values (#{code},#{open},#{close},#{high},#{low},#{volume},#{amount},#{updown},#{change},#{exchange},#{capital},#{date})
+            insert into stock_price(code,open,close,high,low,volume,amount,updown,change,capital,date)
+            values (#{code},#{open},#{close},#{high},#{low},#{volume},#{amount},#{updown},#{change},#{capital},#{date})
             """)
     void insert(StockPricePo stockPricePo);
 }
