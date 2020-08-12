fa795bd34ab7a7f2606508d0b46119918ad89012
shangzheng <shangzheng@pinduoduo.com>
2020-08-12 17:06:03 +0800

    commit

--- src/main/java/com/black/repository/FundPriceRepository.java
+++ src/main/java/com/black/repository/FundPriceRepository.java
@@ -1,49 +1,19 @@
 package com.black.repository;
 
 import com.black.po.FundPricePO;
-import com.black.po.StockInfoPo;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.Date;
-import java.util.List;
 
 @Mapper
 public interface FundPriceRepository {
 
-    @Select("select distinct(code) from stock_info")
-    List<String> queryAllCodes();
-
-    @Insert("""
-            <script>
-            insert into stock_info(code,exchanger,subExchanger,name,biz,openDay,marketDay) values
-            <foreach item="item" index="index" collection="list" open="" separator="," close="">
-                (#{item.code},#{item.exchanger},#{item.subExchanger},#{item.name},#{item.biz}
-                ,#{item.openDay},#{item.marketDay})
-            </foreach> 
-            </script>           
-            """)
-    @Options(useGeneratedKeys = true)
-    void batchInsert(List<StockInfoPo> list);
-
-    @Select("select * from stock_info where infoInit=0")
-    List<StockInfoPo> queryUninitStock();
-
-    @Update("""
-            update stock_info set name=#{name}, biz=#{biz}, openDay=#{openDay}, marketDay=#{marketDay}, infoInit=1
-            where code=#{code}
-            """
-    )
-    void fillInfo(StockInfoPo stockInfoPo);
-
-    @Select("select * from stock_info")
-    List<StockInfoPo> queryAllStocks();
-
-    @Update("update stock_info set ${param1}=#{param2} where id=#{param3}")
-    void updateField(String name, String value, long id);
-
     @Select("select * from fund_code where `date`=#{date} and fundCode=#{fundCode}")
-    FundPricePO queryByDate(@Param("fundCode") String fundCode,@Param("date") Date date);
+    FundPricePO queryByDate(@Param("fundCode") String fundCode, @Param("date") Date date);
 
-    @Insert("insert into fund_info() values()")
+    @Insert("insert into fund_info(fundCode,unit,amount,ratio) values(#{fundCode},#{unit},#{amount},#{ratio})")
     void insert(FundPricePO fundPricePO);
 }
