f655f33eeef743d5e667dc833bb046cc3d8649cb
black-94 <19278347+black-94@users.noreply.github.com>
2020-08-22 07:17:51 +0800

    commit

--- src/main/java/com/black/repository/IpoStockPageRepository.java
+++ src/main/java/com/black/repository/IpoStockPageRepository.java
@@ -1,19 +1,23 @@
 package com.black.repository;
 
 import com.black.po.IpoStockPage;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface IpoStockPageRepository {
 
+    @Delete("truncate table ipo_stock_page")
     void deleteAll();
-
 
     void batchInsert(List<IpoStockPage> ipoStockPages);
 
+    @Select("select distinct(code) from ipo_stock_page")
     List<String> queryAllCodes();
 
+    @Select("select * from ipo_stock_page where code=#{param1}")
     IpoStockPage queryByCode(String code);
 }
