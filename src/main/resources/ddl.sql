265067f4287641660e2f72dcccc109c33520e182
shangzheng <shangzheng@pinduoduo.com>
2020-08-22 13:31:25 +0800

    commit

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,3 +1,89 @@
+
+CREATE TABLE `ipo_stock_page` (
+  `code` varchar(20) NOT NULL COMMENT '股票代码',
+  `name` varchar(50) DEFAULT NULL COMMENT '公司名称',
+  `market_day` varchar(20) DEFAULT NULL COMMENT '上市日期',
+  `market_year` varchar(20) DEFAULT NULL COMMENT '上市年份',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`code`),
+  KEY `ix_market_day` (`market_day`)
+) ;
+
+CREATE TABLE `stock_info_page` (
+  `code` varchar(20) NOT NULL COMMENT '股票代码',
+  `name` varchar(50) DEFAULT NULL COMMENT '公司简名称',
+  `biz` varchar(4000) DEFAULT NULL COMMENT '经营业务',
+  `open_day` varchar(20) DEFAULT NULL COMMENT '成立日期',
+  `market_day` varchar(20) DEFAULT NULL COMMENT '上市日期',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`code`),
+  KEY `ix_market_day` (`market_day`)
+) ;
+
+CREATE TABLE `stock_num_page` (
+  `code` varchar(20) NOT NULL COMMENT '股票代码',
+  `name` varchar(500) DEFAULT NULL COMMENT '公司全名称',
+  `biz` varchar(4000) DEFAULT NULL COMMENT '经营业务',
+  `market_day` varchar(20) DEFAULT NULL COMMENT '上市日期',
+  `total` varchar(50) DEFAULT NULL COMMENT '总股份',
+  `cycle` varchar(50) DEFAULT NULL COMMENT '流通股份',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`code`,`date`),
+  KEY `ix_market_day` (`market_day`)
+) ;
+
+CREATE TABLE `stock_finance_page` (
+  `code` varchar(20) NOT NULL,
+  `income` varchar(50) DEFAULT NULL COMMENT '收入',
+  `profit` varchar(50) DEFAULT NULL COMMENT '利润',
+  `report_day` varchar(20) NOT NULL COMMENT '财报日期',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`code`,`report_day`),
+  KEY `ix_report_day` (`report_day`)
+) ;
+
+CREATE TABLE `stock_price_page` (
+  `code` varchar(20) NOT NULL COMMENT '股票代码',
+  `last_close` varchar(50) DEFAULT NULL COMMENT '昨日收盘价',
+  `open` varchar(50) DEFAULT NULL COMMENT '开盘价',
+  `cur` varchar(50) DEFAULT NULL COMMENT '当前价',
+  `high` varchar(50) DEFAULT NULL COMMENT '最高价',
+  `low` varchar(50) DEFAULT NULL COMMENT '最低价',
+  `volume` varchar(50) DEFAULT NULL COMMENT '交易量',
+  `amount` varchar(50) DEFAULT NULL COMMENT '交易额',
+  `percent` varchar(50) DEFAULT NULL COMMENT '涨跌幅',
+  `change` varchar(50) DEFAULT NULL COMMENT '涨跌额',
+  `date` varchar(20) NOT NULL COMMENT '报告日期',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`code`,`date`),
+  KEY `ix_date` (`date`)
+) ;
+
+CREATE TABLE `stock_price_history_page` (
+  `code` varchar(20) NOT NULL COMMENT '股票代码',
+  `open` varchar(50) DEFAULT NULL COMMENT '开盘价',
+  `high` varchar(50) DEFAULT NULL COMMENT '最高价',
+  `low` varchar(50) DEFAULT NULL COMMENT '最低价',
+  `close` varchar(50) DEFAULT NULL COMMENT '收盘价',
+  `percent` varchar(50) DEFAULT NULL COMMENT '涨跌幅',
+  `change` varchar(50) DEFAULT NULL COMMENT '涨跌额',
+  `amplitude` varchar(50) DEFAULT NULL COMMENT '振幅',
+  `volume` varchar(50) DEFAULT NULL COMMENT '交易量',
+  `amount` varchar(29) DEFAULT NULL COMMENT '交易额',
+  `exchange` varchar(50) DEFAULT NULL COMMENT '换手率',
+  `date` varchar(20) NOT NULL COMMENT '报告日期',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`code`,`date`),
+  KEY `ix_date` (`date`)
+) ;
+
+
+
+
+
+
+
+
 CREATE TABLE `stock_exchanger` (
 	`code` varchar(50)  NOT NULL COMMENT '证券交易所代码',
 	`name` varchar(50)  NOT NULL COMMENT '证券所名称',
