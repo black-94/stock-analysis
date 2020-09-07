24b0303baf7900491a0269d3bf03ee92aefadb85
shangzheng <shangzheng@pinduoduo.com>
2020-09-07 20:40:59 +0800

    commit

--- src/main/java/com/black/jobs/Crawler.java
+++ src/main/java/com/black/jobs/Crawler.java
@@ -25,7 +25,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Collection;
@@ -267,7 +266,7 @@ public class Crawler {
 //
 //    }
 
-    @Scheduled(cron = "0 0 5 * * *")
+    @Scheduled(cron = "0 0 17 * * *")
     public void dayCrawler() {
         queryCurCodes();
         List<String> codes = ipoStockPageRepository.queryAllCodes();
