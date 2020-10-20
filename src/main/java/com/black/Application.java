24ef38243a713e7a1b513e72a2af08eea40913d8
shangzheng <shangzheng@pinduoduo.com>
2020-10-20 20:18:34 +0800

    commit

--- src/main/java/com/black/Application.java
+++ src/main/java/com/black/Application.java
@@ -1,5 +1,6 @@
 package com.black;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
+        log.info("Application begin ----- ");
         Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> LoggerFactory.getLogger("root").error("thread-"+thread.getName(),throwable));
         SpringApplication.run(Application.class);
     }
--- src/main/java/com/black/jobs/Validator.java
+++ src/main/java/com/black/jobs/Validator.java
@@ -1,7 +1,56 @@
 package com.black.jobs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
+
 public class Validator {
+    static Logger validateLogger= LoggerFactory.getLogger("validate");
+    static {
+        validateLogger.info("validate begin ---- ");
+    }
+
+    @Scheduled(cron = "* * * * * ?")
+    public void validateAllPrice(){
+
+//        Instant begin = LocalDate.of(1990, 1, 1).atStartOfDay(ZoneId.systemDefault()).toInstant();
+//        Instant end = LocalDate.now().plus(1, ChronoUnit.DAYS).atStartOfDay(ZoneId.systemDefault()).toInstant();
+//        validatePriceIn(Date.from(begin),Date.from(end));
+    }
+
+
+    public void validatePriceIn(Date begin,Date end){
+
+
+
+
+
+    }
+
+    public void validatePricePage(Date date){
+
+    }
+
+    public void validatePrice(Date date){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
