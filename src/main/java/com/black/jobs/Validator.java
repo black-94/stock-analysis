ffff4d240c47de4ad1b87bfc797c5aba1615fc6b
shangzheng <shangzheng@pinduoduo.com>
2020-10-20 22:53:28 +0800

    commit

--- src/main/java/com/black/jobs/Validator.java
+++ src/main/java/com/black/jobs/Validator.java
@@ -1,7 +1,7 @@
 package com.black.jobs;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,47 +9,33 @@ import java.util.Date;
 
 @Component
 public class Validator {
-    static Logger validateLogger= LoggerFactory.getLogger("validate");
+    static Logger validateLogger = LogManager.getLogger("validate");
+
     static {
         validateLogger.info("validate begin ---- ");
     }
 
     @Scheduled(cron = "* * * * * ?")
-    public void validateAllPrice(){
-
+    public void validateAllPrice() {
+        validateLogger.info("validate begin ---- ");
 //        Instant begin = LocalDate.of(1990, 1, 1).atStartOfDay(ZoneId.systemDefault()).toInstant();
 //        Instant end = LocalDate.now().plus(1, ChronoUnit.DAYS).atStartOfDay(ZoneId.systemDefault()).toInstant();
 //        validatePriceIn(Date.from(begin),Date.from(end));
     }
 
 
-    public void validatePriceIn(Date begin,Date end){
-
-
-
+    public void validatePriceIn(Date begin, Date end) {
 
 
     }
 
-    public void validatePricePage(Date date){
+    public void validatePricePage(Date date) {
 
     }
 
-    public void validatePrice(Date date){
+    public void validatePrice(Date date) {
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
