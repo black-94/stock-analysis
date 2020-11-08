0f756a0b618da85d1dae82b827a8354ed7389aa4
shangzheng <shangzheng@pinduoduo.com>
2020-11-08 16:32:52 +0800

    commit

--- src/main/java/com/black/jobs/Calculator.java
+++ src/main/java/com/black/jobs/Calculator.java
@@ -22,17 +22,23 @@ import com.black.repository.StockPriceHistoryPageRepository;
 import com.black.repository.StockPricePageRepository;
 import com.black.repository.StockQuartlyReportRepository;
 import com.black.util.Helper;
+import com.google.common.collect.Lists;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -40,6 +46,8 @@ import static com.black.util.ExecutorUtil.submit;
 
 @Component
 public class Calculator {
+    static Logger validateLogger = LogManager.getLogger("root.validate.log");
+
     @Autowired
     Finance163Repository finance163Repository;
     @Autowired
@@ -106,6 +114,81 @@ public class Calculator {
         stockInfoPO.setMarketDay(Helper.parseDate(marketDay));
 
         stockInfoRepository.insert(stockInfoPO);
+    }
+
+    public void quartlyInit(String code) {
+        List<StockFinancePage> stockFinancePages = stockFinancePageRepository.queryByCode(code);
+        if (CollectionUtils.isEmpty(stockFinancePages)) {
+            validateLogger.info("quartlyInit finance is empty , code : " + code);
+            return;
+        }
+
+        Collections.sort(stockFinancePages, Comparator.comparing(StockFinancePage::getReportDay));
+
+        List<StockQuartlyReportPO> list = Lists.newArrayList();
+        for (int i = 1; i < stockFinancePages.size(); i++) {
+            StockFinancePage stockFinancePage = stockFinancePages.get(i);
+            StockFinancePage lastFinance = stockFinancePages.get(i - 1);
+            boolean yearBegin = !lastFinance.getReportDay().substring(0, 4).equals(stockFinancePage.getReportDay().substring(0, 4));
+
+            StockQuartlyReportPO reportPO = new StockQuartlyReportPO();
+            reportPO.setCode(stockFinancePage.getCode());
+            reportPO.setReportDay(Helper.parseDate(stockFinancePage.getReportDay()));
+            reportPO.setTotalIncome(Helper.decimalOf(stockFinancePage.getIncome()));
+            reportPO.setTotalProfit(Helper.decimalOf(stockFinancePage.getProfit()));
+            if (yearBegin) {
+                BigDecimal lastProfit = Helper.decimalOf(lastFinance.getProfit());
+                BigDecimal lastIncome = Helper.decimalOf(lastFinance.getIncome());
+                if (!BigDecimal.ZERO.equals(lastIncome) && !BigDecimal.ZERO.equals(lastProfit)
+                        && !BigDecimal.ZERO.equals(reportPO.getTotalIncome()) && !BigDecimal.ZERO.equals(reportPO.getTotalProfit())) {
+                    reportPO.setIncome(reportPO.getTotalIncome().subtract(lastIncome));
+                    reportPO.setProfit(reportPO.getTotalProfit().subtract(lastProfit));
+                }
+            } else {
+                reportPO.setIncome(reportPO.getTotalIncome());
+                reportPO.setProfit(reportPO.getTotalProfit());
+            }
+
+            list.add(reportPO);
+        }
+
+        StockFinancePage stockFinancePage = stockFinancePages.get(0);
+        StockQuartlyReportPO firstReportPo = new StockQuartlyReportPO();
+        firstReportPo.setCode(stockFinancePage.getCode());
+        firstReportPo.setReportDay(Helper.parseDate(stockFinancePage.getReportDay()));
+        firstReportPo.setTotalIncome(Helper.decimalOf(stockFinancePage.getIncome()));
+        firstReportPo.setTotalProfit(Helper.decimalOf(stockFinancePage.getProfit()));
+        firstReportPo.setIncome(firstReportPo.getTotalIncome());
+        firstReportPo.setProfit(firstReportPo.getTotalProfit());
+        list.add(firstReportPo);
+
+        Collections.sort(list, Comparator.comparing(StockQuartlyReportPO::getReportDay));
+
+        for (int i = 1; i < list.size(); i++) {
+            StockQuartlyReportPO reportPO = list.get(i);
+            StockQuartlyReportPO lastReport = list.get(i - 1);
+            LocalDate localDate = reportPO.getReportDay().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate lastDate = lastReport.getReportDay().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            boolean isLast = localDate.plusMonths(-3).getMonthValue() == lastDate.getMonthValue();
+            StockQuartlyReportPO lastYear = list.stream().filter(e -> e.getReportDay().equals(Date.from(localDate.plusYears(-1).atStartOfDay(ZoneId.systemDefault()).toInstant()))).findAny().orElse(null);
+
+            if (isLast && lastReport.getIncome() != null && lastReport.getProfit() != null) {
+                BigDecimal m2mIncome = Helper.calculateIncrease(reportPO.getIncome(), lastReport.getIncome());
+                BigDecimal m2mProfit = Helper.calculateIncrease(reportPO.getProfit(), lastReport.getProfit());
+                reportPO.setM2mIncome(m2mIncome);
+                reportPO.setM2mProfit(m2mProfit);
+            }
+            if (lastYear != null && lastYear.getIncome() != null && lastYear.getProfit() != null) {
+                BigDecimal y2yIncome = Helper.calculateIncrease(reportPO.getIncome(), lastYear.getIncome());
+                BigDecimal y2yProfit = Helper.calculateIncrease(reportPO.getProfit(), lastYear.getProfit());
+                reportPO.setM2mIncome(y2yIncome);
+                reportPO.setM2mProfit(y2yProfit);
+            }
+        }
+
+        stockQuartlyReportRepository.deleteByCode(code);
+        stockQuartlyReportRepository.batchInsert(list);
+
     }
 
 
--- src/main/java/com/black/repository/StockQuartlyReportRepository.java
+++ src/main/java/com/black/repository/StockQuartlyReportRepository.java
@@ -1,11 +1,11 @@
 package com.black.repository;
 
 import com.black.po.StockQuartlyReportPO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -22,4 +22,18 @@ public interface StockQuartlyReportRepository {
             values(#{code},#{income},#{profit},#{totalIncome},#{totalProfit},#{fundRatio},#{m2mIncome},#{m2mProfit},#{y2yIncome},#{y2yProfit},#{reportDay})
             """)
     void insert(StockQuartlyReportPO reportPO);
+
+    @Delete("delete from stock_quartly_report where code=#{param1}")
+    void deleteByCode(String code);
+
+    @Insert("""
+                <script>
+                insert into stock_quartly_report(`code`,`income`,`profit`,`total_income`,`total_profit`,`fund_ratio`,`m2m_income`,`m2m_profit`,`y2y_income`,`y2y_profit`,`report_day`)
+                values
+                <foreach item="item" index="index" collection="param1" open="" separator="," close="">
+                    (#{item.code},#{item.income},#{item.profit},#{item.totalIncome},#{item.totalProfit},#{item.fundRatio},#{item.m2mIncome},#{item.m2mProfit},#{item.y2yIncome},#{item.y2yProfit},#{item.reportDay})
+                </foreach>
+                </script>            
+            """)
+    void batchInsert(List<StockQuartlyReportPO> list);
 }
